a700a9851758841bd4b1ab0c458322cfa23b9f32
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-05-24 15:04:17 +0000

    proyecto generador de cartas finalizado. Versión Básica

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let simbolsCards = ["♦", "♥", "♠", "♣"];
+let numbersCards = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Q",
+  "J",
+  "K"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code her
+  let simbolsCardsRamdom =
+    simbolsCards[Math.floor(Math.random() * simbolsCards.length)];
+  let numbersCardsRamdom =
+    numbersCards[Math.floor(Math.random() * numbersCards.length)];
+
+  let selectorSimbols = document.querySelectorAll(".simbols");
+  for (let element of selectorSimbols) {
+    element.innerHTML = simbolsCardsRamdom;
+    if (simbolsCardsRamdom === "♦" || simbolsCardsRamdom === "♥") {
+      element.style.color = "red";
+    }
+  }
+
+  document.querySelector("#number").innerHTML = numbersCardsRamdom;
 };
